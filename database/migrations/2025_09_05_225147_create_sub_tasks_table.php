c2d7787ec1e10bc2a8af5bbd64c54b4e518c1e43
yuta-git <yuta6234@gmail.com>
2025-09-08 21:45:56 +0900

    サブタスクのCRUD機能の修正②

--- database/migrations/2025_09_05_225147_create_sub_tasks_table.php
+++ database/migrations/2025_09_05_225147_create_sub_tasks_table.php
@@ -1,31 +1,26 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('sub_tasks', function (Blueprint $table) {
-            $table->bigIncrements('id'); // 主キー
+            $table->bigIncrements('id');
             $table->string('name', 30);
             $table->text('content')->nullable();
-            $table->unsignedBigInteger('task_id'); // 外部キー
+            $table->timestamps();
 
-            $table->foreign('task_id')
-                ->references('id')->on('tasks')
+            $table->foreignIdFor(Task::class)
+                ->constrained()
                 ->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('sub_tasks');
